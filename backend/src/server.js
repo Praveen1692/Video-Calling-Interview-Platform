ea839556e2eb7a4ebe0d6c2eb15e4875460103fd
Praveen Sharma <praying.sharma@gmail.com>
2025-11-12 16:41:14 +0530

    Backend Setup

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from "express"
+import dotenv from "dotenv"
+
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000;
+
+
+const app = express();
+
+
+app.get("/health", (req, res) => {
+    return res.status(200).json({ message: "Server running.." })
+})
+
+
+app.listen(PORT, () => {
+    console.log("server is running on port 3000");
+
+})
