61d7ea62946b7d1521a626d0f3a54c2f45063d1b
Praveen Sharma <praying.sharma@gmail.com>
2025-11-12 18:38:16 +0530

    deployment config add

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,11 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import path from "path";
 
 dotenv.config()
 
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve()
 
 
 const app = express();
@@ -13,6 +14,17 @@ const app = express();
 app.get("/health", (req, res) => {
     return res.status(200).json({ message: "Server running.." })
 })
+
+
+// make our app  ready for deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+
+}
 
 
 
